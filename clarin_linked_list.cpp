237faf9a424e03e3cedcd69ff3ef4b8ea7f663bc
mjcarnaje <michaeljamescarnaje1@gmail.com>
2022-09-23 09:46:59 +0800

    clarin linked list update

---
+++ clarin_linked_list.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+  int value;
+  Node *nextNode;
+};
+
+int main()
+{
+  int count;
+
+  cout << "Enter array count: ";
+  cin >> count;
+
+  Node *head = new Node;
+  head->value = 1;
+  head->nextNode = NULL;
+
+  Node *tempNode = head;
+
+  for (int i = 1; i < count; i++)
+  {
+    tempNode->nextNode = new Node;
+    tempNode->nextNode->value = i + 1;
+    tempNode->nextNode->nextNode = NULL;
+    tempNode = tempNode->nextNode;
+  }
+
+  tempNode->nextNode = head;
+  tempNode = head;
+
+  for (int i = 1; i <= count * count; i++)
+  {
+    cout << tempNode->value << " ";
+    if (i % count == 0)
+    {
+      cout << endl;
+    }
+    tempNode = tempNode->nextNode;
+  }
+
+  delete head;
+
+  return 0;
+}
