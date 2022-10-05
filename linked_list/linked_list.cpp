05566791a3161bb2cca0f101425f8c05ff49f6b8
mjcarnaje <michaeljamescarnaje1@gmail.com>
2022-10-05 18:57:00 +0800

    refactor: linked list update

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -13,6 +13,8 @@ int main()
 {
   struct Node *headNode = new Node;
 
+  int COUNT = 3;
+
   assert(headNode != NULL);
 
   headNode->data = 1;
@@ -20,39 +22,39 @@ int main()
 
   Node *tempNode = headNode;
 
-  for (int i = 2; i <= 20; i++)
+  for (int i = 2; i <= COUNT; i++)
   {
     Node *newNode = new Node;
 
     assert(newNode != NULL);
 
     newNode->data = i;
-    newNode->nextNode = NULL;
+    newNode->nextNode = headNode;
 
     tempNode->nextNode = newNode;
     tempNode = newNode;
   }
 
-  // tempNode->nextNode = headNode;
   tempNode = headNode;
 
-  while (tempNode != NULL)
+  for (int i = 1; i <= COUNT * COUNT + 1; i++)
   {
     cout << tempNode->data << " ";
-
     tempNode = tempNode->nextNode;
   }
+
+  cout << endl;
 
   tempNode = headNode;
 
-  while (tempNode != NULL)
+  for (int i = 1; i <= COUNT; i++)
   {
-    Node *deleteNode = tempNode;
-
-    tempNode = tempNode->nextNode;
-
-    delete deleteNode;
+    Node *tempNode2 = tempNode->nextNode;
+    delete tempNode;
+    tempNode = tempNode2;
   }
+
+  cout << "DONE!" << endl;
 
   return 0;
 }
